a9842611872e23f5a2609b3baa4c0836f06b0207
Paul Cantalupo <pcantalupo@gmail.com>
2016-07-20 15:02:14 -0400

    Update samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -3,9 +3,14 @@ bowtie2 ... | samtools view -Sb - > foo.bam
 
 # bowtie2 align and save output as sorted bam (output file will be 'foo.bam')
 bowtie2 ... | samtools view -Sb - | samtools sort - foo
+bowtie2 ... | samtools view -Sb - | samtools sort -o foo.bam # (samtools v1.3)
 
 # convert sam to a sorted bam and index it
 samtools view -Sb foo.sam | samtools sort - foo && samtools index foo.bam
+samtools view -Sb foo.sam | samtools sort -o foo.bam && samtools index foo.bam # (samtools v1.3)
 
 # sort bam in-place and index it
 samtools sort foo.bam foo && samtools index foo.bam
+samtools sort -o foo.bam foo.bam && samtools index foo.bam  # (samtools v1.3)
+
+
