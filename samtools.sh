ed05358212be26bd3d5ffddfad882d3a57eb78df
Paul Cantalupo <pcantalupo@gmail.com>
2016-07-21 11:01:06 -0400

    Update samtools.sh

--- samtools.sh
+++ samtools.sh
@@ -3,12 +3,8 @@ bowtie2 ... | samtools view -Sb - > foo.bam
 
 # bowtie2 align and save output as sorted bam (output file will be 'foo.bam')
 bowtie2 ... | samtools view -Sb - | samtools sort - foo
-bowtie2 ... | samtools view -Sb - | samtools sort -o foo.bam # (samtools v1.3)
-
-# convert sam to a sorted bam and index it
-samtools view -Sb foo.sam | samtools sort - foo && samtools index foo.bam
-samtools view -Sb foo.sam | samtools sort -T foo -o foo.bam && samtools index foo.bam # (samtools v1.3)
+bowtie2 ... | samtools view -Sb - | samtools sort -T foo -o foo.bam # (samtools v1.3; works without -T option on command line but not on SLURM)
 
 # sort bam in-place and index it
 samtools sort foo.bam foo && samtools index foo.bam
-samtools sort -T foo -o foo.bam foo.bam && samtools index foo.bam  # (samtools v1.3)
+samtools sort -T foo -o foo.bam foo.bam && samtools index foo.bam  # (samtools v1.3; works without -T option on command line but not on SLURM)
