3acfe6beff13db5aaae70b280bf5dc4407d6e359
Paul Cantalupo <pcantalupo@gmail.com>
2018-08-03 10:35:54 -0400

    Create ggplot.R

---
+++ R/ggplot.R
@@ -0,0 +1,7 @@
+# Use 'gridExtra::grid.arrange' to place multiple ggplots in a plot
+library(gridExtra)
+plots = lapply(names(mock), function(n) {
+  g = TSNEPlot(mock[[n]], do.label = T, do.return = T)
+  g + ggtitle(paste0("Mock.",n))
+})
+grid.arrange(grobs = plots, ncol = 2)
